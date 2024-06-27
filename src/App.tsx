93244b9002ec1b5ea6efcfe3b7f574bd0ad99bcf
Wachiranan Phuangpanya <wachiranan.pu@gmail.com>
2024-06-27 20:18:00 +0700

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
 import "./App.css";
-import { Box } from "@mui/material";
+import { Box, CssBaseline } from "@mui/material";
 import InputPage from "./components/InputPage";
+import { Navigate, Route, Routes } from "react-router-dom";
+import ResultPage from "./components/resultPage";
+import { useAppSelector } from "./redux/hooks";
 
 function App() {
+  const inputData = useAppSelector((state) => state.inputItems.inputData);
   return (
-    <Box
-      sx={{
-        width: "100%",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        bgcolor: "#DFEDF3",
-      }}
-    >
-      <InputPage />
-    </Box>
+    <>
+      <CssBaseline />
+      <Box
+        sx={{
+          width: "100%",
+          minHeight: "100vh",
+
+          bgcolor: "#DFEDF3",
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Navigate to="/input" />} />
+          <Route path="/input" element={<InputPage />} />
+          <Route
+            path="/result"
+            element={
+              Object.keys(inputData).length !== 0 ? (
+                <ResultPage />
+              ) : (
+                <Navigate to="/input" />
+              )
+            }
+          />
+        </Routes>
+      </Box>
+    </>
   );
 }
 
